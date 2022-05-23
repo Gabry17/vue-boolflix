bd4a4b4f7cdce2c467388229b557bedfd26a69d8
Gabry17 <gbianchi528@gmail.com>
2022-05-23 16:35:32 +0200

    creato componente per l header e gestito ricerca del film

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
     <!-- header -->
-    <header>
-      <div class="title">
-        <h1>netflix</h1>
-      </div>
-      <div class="search">
-        <input type="text" placeholder="titolo film" />
-        <button @click="viewFilm">
-          cerca
-        </button>
-      </div>
-    </header>
+    <AppHeader @search="viewFilm($event)" />
     <!-- /header -->
 
     <!-- main -->
@@ -22,12 +12,14 @@
 
 <script>
 import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue"
 import axios from "axios";
 
 export default {
   name: "App",
   components: {
     AppMain,
+    AppHeader
   },
   data() {
     return {
@@ -35,10 +27,15 @@ export default {
     };
   },
   methods: {
-    viewFilm() {
+    viewFilm(searchTitle) {
       axios
         .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=cda791e52a23bffc861ee9b7107cfc69&query=l"
+          "https://api.themoviedb.org/3/search/movie",{
+            params:{
+              api_key: 'cda791e52a23bffc861ee9b7107cfc69',
+              query: searchTitle
+            }
+          }
         )
         .then((resp) => {
           this.film = resp.data.results;
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+      <div class="title">
+        <h1>netflix</h1>
+      </div>
+      <div class="search">
+        <input v-model="searchTitle" type="text" placeholder="titolo film" />
+        <button @click="$emit('search',searchTitle)">
+          cerca
+        </button>
+      </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            searchTitle: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
