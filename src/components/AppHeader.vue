2500d0361b0eb8cd4f008bc8ae7bff6049223e61
Gabry17 <gbianchi528@gmail.com>
2022-05-23 12:48:47 +0200

    creato header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+      <div class="title">
+          <h1>netflix</h1>
+      </div>
+      <div class="search">
+          <input type="text" placeholder="titolo film">
+          <button>cerca</button>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    background-color: black;
+    padding: 1em;
+    display: flex;
+    justify-content: space-between;
+    line-height: 100px;
+    .title{
+        color: red;
+        text-transform: uppercase;
+    }
+}
+</style>
