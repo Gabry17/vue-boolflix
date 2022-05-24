e8be77f491398dc7d3d71a675bcfb2c431eeaf4c
Gabry17 <gbianchi528@gmail.com>
2022-05-24 12:35:20 +0200

    aggiunto voti da 1 a 5 e completato milestone 3

--- src/components/AppCardFilm.vue
+++ src/components/AppCardFilm.vue
@@ -1,19 +1,52 @@
 <template>
   <div class="card">
-      <img :src="'https://image.tmdb.org/t/p/w342' + filmObj.poster_path" alt="">
+      <img :src="'https://image.tmdb.org/t/p/w342' + filmObj.poster_path" :alt="filmObj.original_title">
       <h3>{{ filmObj.title }}</h3>
       <p>{{ filmObj.original_title }}</p>
-      <p v-if="filmObj.original_language === 'it'">🇮🇹</p>
-      <p v-if="filmObj.original_language === 'en'">🇬🇧</p>
-      <p v-if="filmObj.original_language !== 'it' && filmObj.original_language !== 'en'">{{ filmObj.original_language }}</p>
-      <p>{{ filmObj.vote_average }}</p>
+      <p>{{ lang }}</p>
+      <p>{{ vote }}</p>
   </div>
 </template>
 
 <script>
 export default {
     name: 'AppCardFilm',
-    urlImg: 'https://image.tmdb.org/t/p/w342',
+    data(){
+        return{
+            vote: 0,
+             lang: ''
+        }
+    },
+    mounted() {
+    if (this.filmObj.vote_average > 8) {
+      this.vote = 5;
+    } else if (
+      this.filmObj.vote_average > 6 &&
+      this.filmObj.vote_average < 9
+    ) {
+      this.vote = 4;
+    } else if (
+      this.filmObj.vote_average > 4 &&
+      this.filmObj.vote_average < 7
+    ) {
+      this.vote = 3;
+    } else if (
+      this.filmObj.vote_average > 2 &&
+      this.filmObj.vote_average < 5
+    ) {
+      this.vote = 2;
+    } else {
+      this.vote = 1;
+    }
+
+    if(this.filmObj.original_language === 'it'){
+        this.lang = '🇮🇹'
+    } else if (this.filmObj.original_language === 'en'){
+        this.lang = '🇬🇧'
+    } else {
+        this.lang = this.filmObj.original_language
+    }
+    },
     props: {
         filmObj: Object
     }
--- src/components/AppCardSerie.vue
+++ src/components/AppCardSerie.vue
@@ -1,24 +1,59 @@
 <template>
   <div class="card">
-      <img :src="'https://image.tmdb.org/t/p/w342' + serieObj.poster_path" alt="">
-      <h3>{{ serieObj.name }}</h3>
-      <p>{{ serieObj.original_name }}</p>
-      <p v-if="serieObj.original_language === 'it'">🇮🇹</p>
-      <p v-if="serieObj.original_language === 'en'">🇬🇧</p>
-      <p v-if="serieObj.original_language !== 'it' && serieObj.original_language !== 'en'">{{ serieObj.original_language }}</p>
-      <p>{{ serieObj.vote_average }}</p>
+    <img
+      :src="'https://image.tmdb.org/t/p/w342' + serieObj.poster_path"
+      :alt="serieObj.original_name"
+    />
+    <h3>{{ serieObj.name }}</h3>
+    <p>{{ serieObj.original_name }}</p>
+    <p>{{ lang }}</p>
+    <p>{{ vote }}</p>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'AppCardSerie',
-    props: {
-        serieObj: Object
+  name: "AppCardSerie",
+  data() {
+    return {
+      vote: 0,
+      lang: "",
+    };
+  },
+  props: {
+    serieObj: Object,
+  },
+  mounted() {
+    if (this.serieObj.vote_average > 8) {
+      this.vote = 5;
+    } else if (
+      this.serieObj.vote_average > 6 &&
+      this.serieObj.vote_average < 9
+    ) {
+      this.vote = 4;
+    } else if (
+      this.serieObj.vote_average > 4 &&
+      this.serieObj.vote_average < 7
+    ) {
+      this.vote = 3;
+    } else if (
+      this.serieObj.vote_average > 2 &&
+      this.serieObj.vote_average < 5
+    ) {
+      this.vote = 2;
+    } else {
+      this.vote = 1;
     }
-}
+
+    if(this.serieObj.original_language === 'it'){
+        this.lang = '🇮🇹'
+    } else if (this.serieObj.original_language === 'en'){
+        this.lang = '🇬🇧'
+    } else {
+        this.lang = this.serieObj.original_language
+    }
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
